5e9acc790041de310de0d81982497c519bf15447
Sahad Muhammed Villan <143242622+sahadvillan@users.noreply.github.com>
2026-01-08 13:10:40 +0100

    first commit

---
+++ TEST.py
@@ -0,0 +1,17 @@
+from zenml import pipeline, step
+
+@step
+def ingest() -> int:
+    return 41
+
+@step
+def add_one(x: int) -> int:
+    return x + 1
+
+@pipeline
+def my_pipeline():
+    y = add_one(ingest())
+    return y
+
+if __name__ == "__main__":
+    my_pipeline()
